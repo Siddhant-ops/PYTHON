562086b6b9fe9810c4d94eaa0b075ade01fecbda
Siddhant-ops <siddhantdalvi3@gmail.com>
2020-12-22 16:39:13 +0530

    22/12/2020

--- Studies/new.py
+++ Studies/new.py
@@ -1,15 +1,6 @@
-class Employee:
-    def __init__(self, name, age, gender):
-        self.name - name
-        self.age = age
-        self.gender = gender
-
-    def __del__(self):
-        c_name = self.__class__.__name__
-
-
-e1 = Employee("rajesh", "25", "male")
-e2 = Employee("Prajesh", "30", "Male")
-print(e1.name, e2.age)
-print(e2.name, e1.gender)
-
+sum=0
+n=int(input("Enetr : "))
+while(n ==0):
+    sum = sum + int(n %10)
+    n = n//10
+print(sum)
